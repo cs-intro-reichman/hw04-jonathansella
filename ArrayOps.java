2425116dd6baa5b8e5128d93553a1551d7109766
unknown <jonathansella1@gmail.com>
2024-01-14 18:16:11 +0200

    homework4

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,80 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        System.out.println(findMissingInt(new int[]{1,0,3}));
+        System.out.println(secondMaxValue(new int[] {6, 9, 4, 7, 3, 4}));
+        System.out.println(containsTheSameElements(new int[] {1, 4, 1, 1, 2}, new int[] {2, 1, 4}));
+        System.out.println(isSorted(new int[] {7, 5, 4, 3, -12}));
+        System.out.println(isSorted(new int[] {1, -2, 3}));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        boolean inArray;
+
+        for (int index = 0; index < array.length; index++) {
+            inArray = false; 
+           for (int p = 0; p < array.length; p++) {
+            if (array[p] == index) {
+                inArray = true;
+            }
+           }
+            if (inArray == false) {
+                return index;
+                
+            }
+        }
+        return array.length + 1;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
-    }
+        int max = array[0];
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] >= max) {
+                max = array[index];
+            } 
+        }
+        int max2 = array[0];
+        for (int i = 0; i < array.length; i++) {
+             if (array[i] >= max2 && array[i]!=max) {
+                max2 = array[i];
+             }   
+            }          
+             return max2;
+        }
+         
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
-    }
+        boolean same = false;
+        for (int i = 0; i < array1.length; i++) { 
+            for (int j = 0; j < array2.length; j++) {
+            if (array1[i] == array2 [j]) {
+                same = true; 
+            }
+        } 
+        if (same == false) { 
+            return same; }
+            same = false;
+        }
+    return true;
+    } 
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean same = false;
+        for (int i = 0; i < array.length - 1; i++) { 
+            if (array[i] < array[i+1]) {
+                same = true; 
+            } else { 
+            same = false;
+            break;
+        }
     }
-
+        for (int j = 0; j < array.length - 1; j++) { 
+            if (array[j] > array[j+1]) {
+                same = true; 
+            } else { 
+            same = false;
+            break;
+            }
+        }
+    return same; 
+    }
 }
